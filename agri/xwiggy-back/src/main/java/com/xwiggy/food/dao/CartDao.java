b535adf00b171848d64ae95b862e00696e100fdc
Simon <108006927+SimonEdnz@users.noreply.github.com>
2024-10-20 13:39:17 +0300

    Delete agri directory

--- agri/xwiggy-back/src/main/java/com/xwiggy/food/dao/CartDao.java
+++
@@ -1,7 +0,0 @@
-package com.xwiggy.food.dao;
-
-import com.xwiggy.food.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartDao extends JpaRepository<Cart,Integer> {
-}
